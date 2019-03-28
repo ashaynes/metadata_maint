5d36143542ed22f2db2a37013805bbd2921c183d
Alex Haynes <alexandra.s.haynes@gmail.com>
2019-03-27 23:43:33 -0400

    Adding missing files

---
+++ audio/playbackMusicFile.py
@@ -0,0 +1,83 @@
+import pygame as pg
+import mutagen.id3
+import tkinter.ttk as ttk
+import tkinter
+
+global paused
+
+def playSong(self):
+	# setting events
+	# END_MUSIC_EVENT = pg.USEREVENT + 0
+	# pg.mixer.music.set_endevent(END_MUSIC_EVENT)
+	paused = False
+
+	'''play current song selected in Listbox and update elapsed time'''
+	self.current_song_playing.set(self.song['TIT2'][0])
+	# set Pause and Stop buttons to normal state
+	self.play.config(state=tkinter.DISABLED)
+	self.pause.config(state=tkinter.NORMAL)
+	self.stop.config(state=tkinter.NORMAL)
+	
+	for i in range(0, len(self.all_music)):
+		if self.song['TIT2'][0] in self.all_music[i][1]:
+			time = self.all_music[i][-1]
+	
+	self.lyricsText.config(state=tkinter.NORMAL, cursor="arrow")
+	
+	# display lyrics from USLT tag else display default message
+	lyrics = " -- No lyrics/description to display -- "
+	for tags in self.song.keys():
+		if "USLT" in tags:
+			lyrics = self.song[tags]
+
+	self.lyricsText.delete("0.0", tkinter.END)
+	self.lyricsText.insert("0.0", lyrics)
+	self.lyricsText.config(state=tkinter.DISABLED, cursor="arrow")
+	self.lyricsText.update()
+	
+	# load music file, else return error
+	song_header = self.song.pprint().split("\n")[0].split(", ")
+	freq = "44100 Hz"
+	for x in range(len(song_header)):
+		if "Hz" in song_header[x]:
+			freq = song_header[x]
+			break
+	
+	pg.mixer.pre_init(int(freq[:-3]), -16, 2, 1024)
+	pg.mixer.init()
+	pg.mixer.music.set_volume(0.8)
+	try:
+		pg.mixer.music.load(self.song_path.get())
+		pg.mixer.music.play(1)
+		if pg.mixer.music.get_busy() is True:
+			print (f'{self.song_path.get()} is playing right now')
+	except:
+		pass
+
+	# reset pause status in player
+	if paused is True:
+		pauseSong(self)
+	
+	if pg.mixer.music.get_busy() is False:
+		pg.mixer.music.stop()
+		pg.mixer.music.load("")
+		print (f"{self.song_path.get()} has stopped playing")
+		self.lyricsText.config(state=tkinter.NORMAL, cursor="arrow")	
+	
+def pauseSong(self):
+	'''pauses playback of song'''
+	if pg.mixer.music.get_busy() and self.paused is False:
+		pg.mixer.music.pause()
+		self.pause_unpause.set("Unpause")
+		self.paused = True
+	else:
+		pg.mixer.music.unpause()
+		self.pause_unpause.set("Pause")
+		self.paused = False
+
+def stopSong(self):
+	'''stops the song that is playing'''
+	if pg.mixer.music.get_busy():
+		pg.mixer.music.stop()
+		self.play.config(state=tkinter.NORMAL)
+		self.stop.config(state=tkinter.DISABLED)
---
+++ utils/defaults.py
@@ -0,0 +1,64 @@
+##########################################
+# defaults.py
+#
+# Stores all of the default GUI settings
+# depending on the environment it is
+# being launched
+##########################################
+
+import os
+
+if os.name == "nt":
+    width = 1243
+    height = 705
+    lyricsTextWidth = 57
+    lyricsTextHeight = 50
+    lyricsWinWidth = 405
+    lyricsWinHeight = 520
+    musicLbFrame = 2000
+    musicLbHeight = 15
+    filesWidth = 2500
+    filesHeight = 800
+    renameWidth = 355
+    renameHeight = 75
+    metadataEntryWidth = 81
+    convertBitrateWidth = 425
+    convertBitrateHeight = 80
+    convertWidth2 = 55
+    convertHeightWin = 285
+    convertWidthWin = 500
+    bitrateTitleColWidth = 400
+    bitrateBitrateColWidth = 60
+    editHeight = 2
+    missingMDHeight = 15
+    missingMDWidth = 125
+    corruptMP3Height = 15
+    corruptMP3Width = 125
+    displayWinHeight = 350
+    displayWinWidth = 500
+    moveFileWidth = 315
+    moveFileHeight = 120
+    dispInfoWidth = 800
+    dispInfoHeight = 600
+    convertMP3Width = 800
+    albumartWinHeight = 600
+    albumartWinWidth = 800
+
+if os.name == "posix":
+    width = 1255
+    height = 705
+    lyricsColumn = 7
+    lyricsTextWidth = 40
+    lyricsTextHeight = 18
+    filesWidth = 5000
+    filesHeight = 500
+    renameWidth = 400
+    renameHeight = 75
+    metadataEntryWidth = 60
+    convertWidth1 = 490
+    convertWidth2 = 45
+    convertHeightWin = 265
+    bitrateColWidth = 50
+    editHeight = 3
+    missingMDHeight = 17
+    missingMDWidth = 106
